4a8be3a86e3a9ae73cd4b68452ffead68042776a
k042330 <126170992+k042330@users.noreply.github.com>
2025-03-07 01:11:13 +0700

    Update mis2.py

--- mis2.py
+++ mis2.py
@@ -310,4 +310,5 @@ with open('templates/index.html', 'w', encoding='utf-8') as f:
     ''')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
